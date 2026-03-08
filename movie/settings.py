f30d3e686cba65c505b6a00963ed113acc62af44
thaisothun <thaisothun@gmail.com>
2026-03-08 16:41:56 +0700

    update

--- movie/settings.py
+++ movie/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['moviekt.up.railway.app','localhost', '127.0.0.1','www.4allmovie.com','4allmovie.com']
 CSRF_TRUSTED_ORIGINS = ['https://moviekt.up.railway.app','https://www.4allmovie.com','https://4allmovie.com']
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
