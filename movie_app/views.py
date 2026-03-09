666fe4ae928aeff5e21507b1b2f5f2d80e5b0e75
thaisothun <thaisothun@gmail.com>
2026-03-09 11:41:51 +0700

    update

--- movie_app/views.py
+++ movie_app/views.py
@@ -19,7 +19,6 @@ def csrf(request):
 def index(request):
     return redirect('home')
 
-@ensure_csrf_cookie
 def browse():
     types = MovieType.objects.all()
     current_year = date.today().year +1
