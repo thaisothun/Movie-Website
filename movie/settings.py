0b1dae76a66976308e88750cb6ae5f475c6be83e
thaisothun <thaisothun@gmail.com>
2026-03-16 11:03:43 +0700

    update

--- movie/settings.py
+++ movie/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['moviekt.up.railway.app','localhost', '127.0.0.1','www.4allmovie.com','4allmovie.com']
 CSRF_TRUSTED_ORIGINS = ['https://moviekt.up.railway.app','https://*.4allmovie.com','https://4allmovie.com']
